dd5e6abe422512c4ea443d9a62efa945d6ed2568
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-11-28 10:20:22 +0530

    feat(client): added client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [transaction, setTransaction] = useState([]);
+
+const loadTransaction = async () => {
+  const response = await axios.get('/api/transactions');
+
+  const alldata = response?.data?.data ;
+  console.log(alldata)
+  setTransaction(alldata);
 }
 
-export default App;
+useEffect(() => {
+loadTransaction()
+},[] )
+  return (
+    <>
+    <div>
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
+
